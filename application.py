5e9c655be959bd84633223d66c1c24afa07cdf6c
github-nicpoz <30658758+github-nicpoz@users.noreply.github.com>
2022-11-17 18:38:02 +0100

    Vers 2

--- application.py
+++ application.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<html><body><h1>Hello Best Bike App from GitHub - Version 1!</h1></body></html>\n"
+    return "<html><body><h1>Hello Best Bike App from GitHub - Version 2!</h1></body></html>\n"
