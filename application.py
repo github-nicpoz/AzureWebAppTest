6563533506d0ef114506d4b1be642e3a61e03274
github-nicpoz <30658758+github-nicpoz@users.noreply.github.com>
2023-12-05 17:46:50 +0100

    Update application.py 2023 ver 1.0

--- application.py
+++ application.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<html><body><h1>Hello Best Bike App from GitHub - Version 2023 </h1></body></html>\n"
+    return "<html><body><h1>Hello Best Bike App from GitHub - Version 2023 Ver 1.0</h1></body></html>\n"
